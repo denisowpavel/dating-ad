5dc4270e202bb2c943e22cdf699699afd9831ad8
Pavel Denisov <pv.denisov@interfax.ru>
2022-11-11 21:41:21 +0100

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+  TuiModeModule, TuiButtonModule, TuiSvgModule,
+} from '@taiga-ui/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {TuiActionModule, TuiInputModule, TuiMarkerIconModule, TuiToggleModule} from '@taiga-ui/kit';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiModeModule,
+    TuiActionModule,
+    TuiButtonModule,
+    TuiInputModule,
+    TuiToggleModule,
+    TuiMarkerIconModule,
+    TuiSvgModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
